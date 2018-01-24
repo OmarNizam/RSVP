ef938db9e2db4c15b3dacfe629b9e1306c680880
OmarNizam <eng.omarnizam@gmail.com>
2018-01-24 02:20:31 +0100

    Removing Names From the List

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -14,7 +14,7 @@ const GuestList = props =>
           handleConfirmation={() => props.toggleConfirmationAt(index)}
           handleToggleEditing={() => props.toggleEditingAt(index)}
           setName={text => props.setNameAt(text, index)}
-        />
+          handleRemove={() => props.removeGuestAt(index)} />
 
     )}
   </ul>;
@@ -25,6 +25,7 @@ GuestList.propTypes = {
   toggleEditingAt: PropTypes.func.isRequired,
   setNameAt: PropTypes.func.isRequired,
   isFiltered: PropTypes.bool.isRequired,
+  removeGuestAt: PropTypes.func.isRequired,
 }
 
 export default GuestList;
