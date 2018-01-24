25dfe5433386ce2fb2cafbe17eb5f6d353859d1c
OmarNizam <eng.omarnizam@gmail.com>
2018-01-24 02:56:59 +0100

    Creating the Counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import GuestList from './components/GuestList';
-
+import Counter from './components/Counter';
 
 class App extends Component {
 
@@ -94,10 +94,17 @@ class App extends Component {
   }
 
   getTotalInvited = () => this.state.guests.length;
-  // getAttendingGuests = () =>
+
+  getAttendingGuests = () =>
+    this.state.guests.reduce((total, guest) =>
+      guest.isConfirmed ? total + 1 : total
+    , 0);
   // getUnconfirmedGuests = () =>
 
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getAttendingGuests();
+    const numberUncofirmed = totalInvited - numberAttending;
     return (
       <div className="App">
         <header>
@@ -129,22 +136,9 @@ class App extends Component {
             </label>
           </div>
 
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+          <Counter totalInvited={totalInvited}
+                   numberAttending={numberAttending}
+                   numberUncofirmed={numberUncofirmed} />
 
           <GuestList
             guests={this.state.guests}
